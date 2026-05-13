673acc9f4e1156b94d68cbd2d3b2d4181f6a3d7d
Massosisho <miguel.masso@estudiantat.upc.edu>
2026-05-13 20:43:51 +0200

    Practica 5 Parte 1

---
+++ src/main.cpp
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+void setup()
+{
+    Wire.begin(20, 21);  // SDA, SCL              // Inicializa I2C
+    Serial.begin(115200);      // Comunicación serie
+
+    while (!Serial)
+    {
+        ; // Espera al monitor serie
+    }
+
+    Serial.println("\nI2C Scanner");
+}
+
+void loop()
+{
+    byte error, address;
+    int nDevices = 0;
+
+    Serial.println("Scanning...");
+
+    for (address = 1; address < 127; address++)
+    {
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+
+        if (error == 0)
+        {
+            Serial.print("I2C device found at address 0x");
+            if (address < 16)
+                Serial.print("0");
+
+            Serial.print(address, HEX);
+            Serial.println(" !");
+            nDevices++;
+        }
+        else if (error == 4)
+        {
+            Serial.print("Unknown error at address 0x");
+            if (address < 16)
+                Serial.print("0");
+
+            Serial.println(address, HEX);
+        }
+    }
+
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("done\n");
+
+    delay(5000); // Espera 5 segundos
+}
